3c00f1f020b2ce91dc2031447a7d02c898defc4f
Kr. SOURAV <Kr.SOURAV250@GmaiL.CoM>
2024-05-03 15:06:20 +0530

    Static files

--- fees/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from fees import views
-
-urlpatterns = [
-    path(route="pyfee", view=views.python_fees, name="pyfee"),
-    path(route="djfee", view=views.django_fees, name="djfee"),
-]

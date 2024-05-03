8bb21b11ec1139fbd16325f227e2c9ff28ab79fe
Kr. SOURAV <Kr.SOURAV250@GmaiL.CoM>
2024-05-03 16:04:00 +0530

    Static files

--- course/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from course import views
-
-urlpatterns = [
-    path(route="learnpy", view=views.learn_python, name="learnPython"),
-    path(route="learndj", view=views.learn_django, name="learnDjango"),
-]
